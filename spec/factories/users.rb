676ef3cf9b7bec03c5d8963c3d80bbff3d5fd47e
Joshua Waiswa <jwaiswa7@gmail.com>
2021-03-15 21:36:02 +0300

    User model specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    imei { Faker::Code.imei }
+    nin { Faker::Code.imei }
+    infected { false }
+    status { "green" } 
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user ) { FactoryBot.create :user }
+
+  it "checks valid record" do 
+  	 expect(user).to be_valid
+  end
+
+  it "status is valid" do 
+  	user.status = "black"
+  	expect(user).to_not be_valid
+  end
 end
